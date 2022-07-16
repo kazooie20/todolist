c8e3bb1ab14d4bd81c5ecad4d51577f68031a878
unknown <rayner_tan@rocketmail.com>
2022-07-16 22:30:21 +0800

    Added date functionality v1.1

---
+++ app.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+
+
+app.get('/', (req,res) => {
+    const currentDay = get_date();
+    if (currentDay === 6 || currentDay === 0) {
+        res.send('Weekend, boo!');
+    }
+    else {
+        res.send('Weekdays, YAY');
+    }
+    
+})
+
+app.listen(3000, () => {
+    console.log('Server running on port 3000');
+});
+
+const get_date = () => {
+    const today = new Date ();
+    const day = today.getDay();
+    return day;
+}
